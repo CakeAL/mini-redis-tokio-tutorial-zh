7fad254795141c5a76e0be9f9f49ec249961fe3e
CakeAL <1742507190@qq.com>
2024-05-18 18:38:18 +0800

    ✨ feat(HelloTokio): 代码更新 Hello Tokio 章节

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // 对mini-redis的地址打开一个链接
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // 设置key "hello" 的value为 "world"
+    client.set("hello", "world".into()).await?;
+
+    // 获取key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
 }
