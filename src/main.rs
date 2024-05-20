ef26e398fc6a24609f334c8830422b0fc99379e3
CakeAL <1742507190@qq.com>
2024-05-20 09:59:47 +0800

    ✨ feat(Shared state): 代码更新 Shared state 章节

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // 把listener绑定到这个地址
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // 第二个值包含了新连接的IP和端口
         let (socket, _) = listener.accept().await.unwrap();
+        // Clone the handle to the hash map
+        // 克隆hash map的句柄
+        let db = db.clone();
+
+        println!("Accepted");
         // 每个传入的套接字都会被生成一个新任务。套接字（的所有权）
         // 被移动到新任务并且在那儿处理。
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // HashMap用来存储值
-    let mut db = HashMap::new();
 
     // 由 `mini-redis` 提供的 Connection ，处理解析从套接字传过来的帧
     // (handles parsing frames from the socket)
@@ -32,16 +42,14 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // 值被存储为 `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` 期望数据类型是 `Bytes`。
-                    // 这种类型在本教程中稍后介绍。现在，
-                    // `&Vec<u8>` 可以使用 `into()` 转换为 `Bytes`。
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
